9a71b7ba25222bea874a2110b7457406b07d20ba
ChaosKai <kai-waggeling@web.de>
2018-06-20 01:58:46 +0200

    Update lss.js

--- userscript/lss.js
+++ userscript/lss.js
@@ -1,5 +1,16 @@
-var peer = new Peer({secure: true});
+var p = new Peer({ initiator: false, trickle: false })
 
-peer.on('open', function(id) {
-    console.log('My peer ID is: ' + id);
-});
+p.on('error', function (err) { console.log('error', err) })
+
+p.on('signal', function (data) {
+  console.log('SIGNAL', JSON.stringify(data))
+})
+
+p.on('connect', function () {
+  console.log('CONNECT')
+  p.send('whatever' + Math.random())
+})
+
+p.on('data', function (data) {
+  console.log('data: ' + data)
+})
