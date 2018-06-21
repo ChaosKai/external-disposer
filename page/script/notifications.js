bc4af532f0aba8077ca2dfdfe8d3550642fd7026
ChaosKai <kai-waggeling@web.de>
2018-06-22 01:39:42 +0200

    Create notifications.js

---
+++ page/script/notifications.js
@@ -0,0 +1,40 @@
+
+
+    var notification_style_link  = document.createElement('link');
+    notification_style_link.id   = cssId;
+    notification_style_link.rel  = 'stylesheet';
+    notification_style_link.type = 'text/css';
+    notification_style_link.href = 'http://website.com/css/stylesheet.css';
+    document.body.appendChild(notification_style_link);
+
+
+    $("body").appendChild('<section id="notifications"></section>');
+
+    var notification = {
+    
+        index: 0,
+        
+        create: function(type, title, message, time)
+        {
+            $("#notifications").append('<div id="notification-' + notification.index + '" class="' + type + '"></div>');
+            $("#notification-" + notification.index).append('<div class="type"></div>');
+            $("#notification-" + notification.index).append('<div class="message">' + message + '</div>');
+            
+            $("#notification-" + notification.index).css({opacity:"1.0"});
+            $("#notification-" + notification.index).animate({opacity:"1.0"}, () => {
+                setTimeout((n_index) => {
+                    $("#notification-" + notification.index).animate({opacity:"0.0"}, () => {
+                        $("#notification-" + notification.index).remove();
+                    });
+                }, time, notification.index);
+            });
+            
+            notification.index++;
+        },
+        
+        close: function(element)
+        {
+            
+        }
+        
+    };
