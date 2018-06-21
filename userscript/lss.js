6aefca4e45a7b5d90659fb2170832b2c4d1d1827
ChaosKai <kai-waggeling@web.de>
2018-06-21 04:09:16 +0200

    Update lss.js

--- userscript/lss.js
+++ userscript/lss.js
@@ -1,20 +1,5 @@
 var p = null;
 
-p.on('error', function (err) { console.log('error', err) })
-
-p.on('signal', function (data) {
-  console.log('SIGNAL', JSON.stringify(data))
-})
-
-p.on('connect', function () {
-  console.log('CONNECT')
-  p.send('whatever' + Math.random())
-})
-
-p.on('data', function (data) {
-  console.log('data: ' + data)
-})
-
 
 $('#news_li').before('<li id="disposer_dropdown" class="dropdown"></li>');
     $('#disposer_dropdown').append('<a href="#" role="button" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-ok" style="margin-right: 8px; color: #FFFFFF"></span>Disposer</a>');
