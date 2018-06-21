9930e1209f998071ef480b64b1f8ab89c8af6337
ChaosKai <kai-waggeling@web.de>
2018-06-21 03:53:17 +0200

    Update lss.js

--- userscript/lss.js
+++ userscript/lss.js
@@ -17,3 +17,9 @@ p.on('connect', function () {
 p.on('data', function (data) {
   console.log('data: ' + data)
 })
+
+
+$('#news_li').before('<li id="disposer_dropdown" class="dropdown"></li>');
+    $('#disposer_dropdown').append('<a href="#" role="button" class="dropdown-toggle" data-toggle="dropdown"><span id="tableau_glyph" class="glyphicon glyphicon-ok" style="margin-right: 8px; color: #FFFFFF"></span>Disposer</a>');
+    $('#disposer_dropdown').append('<ul class="dropdown-menu" role="menu"></ul>');
+        $('#disposer_dropdown').find(".dropdown-menu").append('<li role="presentation"><a id="tableau_state">Verbinden</a></li>');
